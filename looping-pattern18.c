04646348c0bbc917624b57dfdc31c34951fbc457
yashmakadia773 <133627880+yashmakadia773@users.noreply.github.com>
2023-08-16 08:55:51 +0530

    Add files via upload

---
+++ looping-pattern18.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+
+main()
+{
+		int i,j ;
+		
+		for(i=5; i>=1; i--)
+		{	
+			for(j=5; j>=i; j--)
+			{
+				if(j%2==1)
+				{
+					printf("0");
+				}
+				else
+				{	
+					printf("1");
+				}		
+			}		
+			printf("\n");
+		}		
+		
+}
+		
+
+	
+	
